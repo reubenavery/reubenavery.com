af10d93fe6a5f69a678362cea0537915050bbae2
Reuben Avery <reubenavery@gmail.com>
2014-03-22 17:42:28 -0400

    Tweaks for epiceditor

--- html/sites/all/modules/contrib/wysiwyg/editors/js/epiceditor.js
+++ html/sites/all/modules/contrib/wysiwyg/editors/js/epiceditor.js
@@ -7,14 +7,14 @@ Drupal.wysiwyg.editor.attach.epiceditor = function (context, params, settings) {
   var $target = $('#' + params.field);
   var containerId = params.field + '-epiceditor';
   var defaultContent = $target.val();
-  $target.hide().after('<div id="' + containerId + '" />');
+  $target.hide().after('<div id="' + containerId + '" class="epiceditor-widget-wrapper" />');
 
   settings.container = containerId;
   settings.file = {
     defaultContent: defaultContent
   };
   settings.theme = {
-    preview: '/themes/preview/preview-dark.css',
+    preview: '/themes/preview/github.css',
     editor: '/themes/editor/' + settings.theme + '.css'
   }
   var editor = new EpicEditor(settings).load();
@@ -30,9 +30,14 @@ Drupal.wysiwyg.editor.detach.epiceditor = function (context, params, trigger) {
 
   $target.val(editor.exportFile());
 
-  editor.unload(function () {
-    $target.show();
-  });
+  // Clean-up after itself.
+  $target.closest('div.form-item').find('.epiceditor-widget-wrapper').remove();
+
+  if (trigger === 'unload') {
+    editor.unload(function () {
+      $target.show();
+    });
+  }
 };
 
 })(jQuery);
