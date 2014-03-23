06fe3734e713c1a2a76b62163ee46246eb7097d2
Reuben Avery <reubenavery@gmail.com>
2014-03-22 23:14:05 -0400

    Nightly checkin

--- html/sites/all/themes/studio/template.php
+++ html/sites/all/themes/studio/template.php
@@ -5,3 +5,12 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Studio theme.
  */
+
+function studio_wysiwyg_editor_settings_alter(&$settings, $context) {
+  if ($context['editor']['name'] == 'epiceditor') {
+    $settings['theme'] = 'epic-light';
+    $settings['autogrow'] = (Object) Array(
+      'minHeight' => 300
+    );
+  }
+}
